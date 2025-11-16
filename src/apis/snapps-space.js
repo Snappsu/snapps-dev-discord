8c816c3b180c5f0bb6db15c9237bc1876ca1c1f8
Snapps <snapples64@gmail.com>
2025-11-16 02:26:57 -0800

    get own profile is not needed

--- src/apis/snapps-space.js
+++ src/apis/snapps-space.js
@@ -10,25 +10,3 @@ export async function getUser(discordID) {
 		console.error(error);
 	}
 }
-
-export async function getOwnProfile(discordID) {
-	console.log(`Getting Snapps' Space User of Discord User ${discordID}...`)
-	const url = `https://shibboleth.snapps.dev/lookup/discord-id?value=${discordID}`;
-	const options = {
-		method: 'GET'
-	};
-
-	try {
-		const data = await env.shibboleth.BOT_getOwnProfile(discordID)
-    console.log(`Profile info recieved!`)
-    //console.log(data)
-
-		return {
-			response: data.status,
-			data: data
-		}
-	} catch (error) {
-    console.error(`Error getting profile info!`)
-		console.error(error);
-	}
-}
